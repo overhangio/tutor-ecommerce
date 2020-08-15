27caec7f2bb16537562d2c55481cb194d7338ee2
Régis Behmo <regis@behmo.com>
2020-08-15 18:48:04 +0200

    Serve assets with whitenoise
    This drastically simplifies asset serving.

--- tutorecommerce/templates/ecommerce/apps/ecommerce/settings/production.py
+++ tutorecommerce/templates/ecommerce/apps/ecommerce/settings/production.py
@@ -2,6 +2,9 @@ from ..production import *
 
 {% include "ecommerce/apps/ecommerce/settings/partials/common.py" %}
 
+MIDDLEWARE = list(MIDDLEWARE)
+MIDDLEWARE.insert(0, "whitenoise.middleware.WhiteNoiseMiddleware")
+
 SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "{% if ACTIVATE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}"
 
 BACKEND_SERVICE_EDX_OAUTH2_KEY = "{{ ECOMMERCE_OAUTH2_KEY }}"
