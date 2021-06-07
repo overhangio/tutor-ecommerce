7f59573c95ceb44d482b08e1532f54b1ee6112d6
Régis Behmo <regis@behmo.com>
2021-06-07 10:00:49 +0200

    feat: upgrade to Lilac

--- tutorecommerce/templates/ecommerce/apps/ecommerce/settings/development.py
+++ tutorecommerce/templates/ecommerce/apps/ecommerce/settings/development.py
@@ -2,6 +2,13 @@ from ..devstack import *
 
 {% include "ecommerce/apps/ecommerce/settings/partials/common.py" %}
 
+CORS_ORIGIN_WHITELIST = list(CORS_ORIGIN_WHITELIST) + [
+    "http://{{ MFE_HOST }}:{{ ECOMMERCE_MFE_APP['port'] }}",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://{{ MFE_HOST }}:{{ ECOMMERCE_MFE_APP['port'] }}",
+]
+
 SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "http://{{ LMS_HOST }}:8000"
 
 BACKEND_SERVICE_EDX_OAUTH2_KEY = "{{ ECOMMERCE_OAUTH2_KEY_DEV }}"
