f66fd829c18bba0d0f6a46fd8c1599630b52dbf1
lpm0073 <lpm0073@gmail.com>
2022-05-15 13:13:58 -0500

    refactor plugin.py, add a build hook to clone custom theme repo if it exists

--- tutorecommerce/plugin.py
+++ tutorecommerce/plugin.py
@@ -1,14 +1,54 @@
 from glob import glob
 import os
+import pkg_resources
+
+from tutor import hooks as tutor_hooks
 
 from .__about__ import __version__
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-templates = os.path.join(HERE, "templates")
-
 config = {
-    "add": {
+    "defaults": {
+        "VERSION": __version__,
+        "API_TIMEOUT": 5,
+        "CURRENCY": "USD",
+        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-ecommerce:{{ ECOMMERCE_VERSION }}",
+        "WORKER_DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-ecommerce-worker:{{ ECOMMERCE_VERSION }}",
+        "EXTRA_PIP_REQUIREMENTS": [],
+        "HOST": "ecommerce.{{ LMS_HOST }}",
+        "MYSQL_DATABASE": "ecommerce",
+        "MYSQL_USERNAME": "ecommerce",
+        "OAUTH2_KEY": "ecommerce",
+        "OAUTH2_KEY_DEV": "ecommerce-dev",
+        "OAUTH2_KEY_SSO": "ecommerce-sso",
+        "OAUTH2_KEY_SSO_DEV": "ecommerce-sso-dev",
+        "CUSTOM_THEME_REPOSITORY_URL": "",
+        "CUSTOM_THEME_NAME": "edx-theme",
+        "WORKER_JWT_ISSUER": "ecommerce-worker",  # TODO do we need to keep this?
+        # Micro frontend applications
+        "MFE_APP": {
+            "name": "orders",
+            "repository": "https://github.com/edx/frontend-app-ecommerce",
+            "port": 1996,
+        },
+        "PAYMENT_MFE_APP": {
+            "name": "payment",
+            "repository": "https://github.com/edx/frontend-app-payment",
+            "port": 1998,
+            "env": {
+                "production": {
+                    # Hardcoded in edx-platform
+                    "CURRENCY_COOKIE_NAME": "edx-price-l10n",
+                    # TODO set customizable value
+                    "CYBERSOURCE_URL": "https://testsecureacceptance.cybersource.com/silent/pay",
+                    "SUPPORT_URL": "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}/support",
+                },
+                "development": {
+                    "SUPPORT_URL": "http://{{ LMS_HOST }}:8000/support",
+                },
+            },
+        },
+    },
+    "unique": {
         "MYSQL_PASSWORD": "{{ 8|random_string }}",
         "SECRET_KEY": "{{ 20|random_string }}",
         "OAUTH2_SECRET": "{{ 8|random_string }}",
@@ -37,65 +77,108 @@ config = {
         "EXTRA_PAYMENT_PROCESSOR_CLASSES": [],
         "EXTRA_PAYMENT_PROCESSOR_URLS": {},
     },
-    "defaults": {
-        "VERSION": __version__,
-        "API_TIMEOUT": 5,
-        "CURRENCY": "USD",
-        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-ecommerce:{{ ECOMMERCE_VERSION }}",
-        "WORKER_DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-ecommerce-worker:{{ ECOMMERCE_VERSION }}",
-        "EXTRA_PIP_REQUIREMENTS": [],
-        "HOST": "ecommerce.{{ LMS_HOST }}",
-        "MYSQL_DATABASE": "ecommerce",
-        "MYSQL_USERNAME": "ecommerce",
-        "OAUTH2_KEY": "ecommerce",
-        "OAUTH2_KEY_DEV": "ecommerce-dev",
-        "OAUTH2_KEY_SSO": "ecommerce-sso",
-        "OAUTH2_KEY_SSO_DEV": "ecommerce-sso-dev",
-        "WORKER_JWT_ISSUER": "ecommerce-worker",  # TODO do we need to keep this?
-        # Micro frontend applications
-        "MFE_APP": {
-            "name": "orders",
-            "repository": "https://github.com/edx/frontend-app-ecommerce",
-            "port": 1996,
-        },
-        "PAYMENT_MFE_APP": {
-            "name": "payment",
-            "repository": "https://github.com/edx/frontend-app-payment",
-            "port": 1998,
-            "env": {
-                "production": {
-                    # Hardcoded in edx-platform
-                    "CURRENCY_COOKIE_NAME": "edx-price-l10n",
-                    # TODO set customizable value
-                    "CYBERSOURCE_URL": "https://testsecureacceptance.cybersource.com/silent/pay",
-                    "SUPPORT_URL": "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}/support",
-                },
-                "development": {
-                    "SUPPORT_URL": "http://{{ LMS_HOST }}:8000/support",
-                },
-            },
-        },
+    "overrides": {
     },
 }
 
-hooks = {
-    "build-image": {
-        "ecommerce": "{{ ECOMMERCE_DOCKER_IMAGE }}",
-        "ecommerce-worker": "{{ ECOMMERCE_WORKER_DOCKER_IMAGE }}",
-    },
-    "remote-image": {
-        "ecommerce": "{{ ECOMMERCE_DOCKER_IMAGE }}",
-        "ecommerce-worker": "{{ ECOMMERCE_WORKER_DOCKER_IMAGE }}",
-    },
-    "init": ["mysql", "lms", "ecommerce"],
-}
+################# Initialization tasks
+tutor_hooks.Filters.COMMANDS_INIT.add_item(
+    (
+        "lms",
+        ("ecommerce", "hooks", "lms", "init"),
+    )
+)
+tutor_hooks.Filters.COMMANDS_INIT.add_item(
+    (
+        "ecommerce",
+        ("ecommerce", "hooks", "ecommerce", "init"),
+    )
+)
+tutor_hooks.Filters.COMMANDS_INIT.add_item(
+    (
+        "mysql",
+        ("ecommerce", "hooks", "mysql", "init"),
+    )
+)
+tutor_hooks.Filters.IMAGES_BUILD.add_item(
+    (
+        "ecommerce",
+        ("ecommerce", "hooks", "ecommerce", "build"),
+        ("plugins", "ecommerce", "build", "ecommerce"),
+        "{{ ECOMMERCE_DOCKER_IMAGE }}",
+        (),
+    )
+)
+tutor_hooks.Filters.IMAGES_BUILD.add_item(
+    (
+        "ecommerce-worker",
+        ("plugins", "ecommerce", "build", "ecommerce-worker"),
+        "{{ ECOMMERCE_WORKER_DOCKER_IMAGE }}",
+        (),
+    )
+)
+
+@tutor_hooks.Filters.IMAGES_PULL.add()
+@tutor_hooks.Filters.IMAGES_PUSH.add()
+def _add_remote_ecommerce_image_iff_customized(images, user_config):
+    """
+    Register ECOMMERCE image for pushing & pulling if and only if it has
+    been set to something other than the default.
+
+    This is work-around to an upstream issue with ECOMMERCE config. Briefly:
+    User config is baked into ECOMMERCE builds, so Tutor cannot host a generic
+    pre-built ECOMMERCE image. Howevever, individual Tutor users may want/need to
+    build and host their own ECOMMERCE image. So, as a compromise, we tell Tutor
+    to push/pull the ECOMMERCE image if the user has customized it to anything
+    other than the default image URL.
+    """
+    image_tag = user_config["ECOMMERCE_DOCKER_IMAGE"]
+    if not image_tag.startswith("docker.io/overhangio/openedx-ecommerce:"):
+        # Image has been customized. Add to list for pulling/pushing.
+        images.append(("ecommerce", image_tag))
+
+    image_tag = user_config["ECOMMERCE_WORKER_DOCKER_IMAGE"]
+    if not image_tag.startswith("docker.io/overhangio/openedx-ecommerce-worker:"):
+        # Image has been customized. Add to list for pulling/pushing.
+        images.append(("ecommerce-worker", image_tag))
+
+    return images
 
 
-def patches():
-    all_patches = {}
-    for path in glob(os.path.join(HERE, "patches", "*")):
-        with open(path) as patch_file:
-            name = os.path.basename(path)
-            content = patch_file.read()
-            all_patches[name] = content
-    return all_patches
+################# You don't really have to bother about what's below this line,
+################# except maybe for educational purposes :)
+
+# Plugin templates
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    pkg_resources.resource_filename("tutorecommerce", "templates")
+)
+tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("ecommerce/build", "plugins"),
+        ("ecommerce/apps", "plugins"),
+    ],
+)
+# Load all patches from the "patches" folder
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutorecommerce", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+# Load all configuration entries
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        (f"ECOMMERCE_{key}", value)
+        for key, value in config["defaults"].items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        (f"ECOMMERCE_{key}", value)
+        for key, value in config["unique"].items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
