2ca25465cfd8f74a52430479af5a5f6f1c2eee10
Muhammad Faraz  Maqsood <faraz.maqsood@a006-01130.home>
2024-05-15 11:02:04 +0500

    feat: upgrade to redwood

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ setup(
     include_package_data=True,
     python_requires=">=3.8",
     install_requires=[
-        "tutor>=17.0.0,<18.0.0",
-        "tutor-discovery>=17.0.0,<18.0.0",
-        "tutor-mfe>=17.0.0,<18.0.0",
+        "tutor>=18.0.0,<19.0.0",
+        "tutor-discovery>=18.0.0,<19.0.0",
+        "tutor-mfe>=18.0.0,<19.0.0",
     ],
-    extras_require={"dev": ["tutor[dev]>=17.0.0,<18.0.0"]},
+    extras_require={"dev": ["tutor[dev]>=18.0.0,<19.0.0"]},
     entry_points={"tutor.plugin.v1": ["ecommerce = tutorecommerce.plugin"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
